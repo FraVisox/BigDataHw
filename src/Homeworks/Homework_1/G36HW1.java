4430d024bcbaf711b9aac74a24324a75a1a093f7
Francesco Visona <visona.francesco@gmail.com>
2025-04-03 09:29:42 +0200

    Initialized everything but the 3 functions

--- src/Homeworks/Homework_1/G36HW1.java
+++ src/Homeworks/Homework_1/G36HW1.java
@@ -1,17 +1,21 @@
-package BigDataHw.Homework_1;
+package Homeworks.Homework_1;
 
+import algebra.lattice.Bool;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
+import org.apache.spark.mllib.linalg.Vectors;
+import scala.Array;
 import scala.Tuple2;
+import org.apache.spark.mllib.linalg.Vector;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
+
 
 public class G36HW1 {
     public static void main(String[] args) {
@@ -45,21 +49,29 @@ public class G36HW1 {
         // INPUT READING
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        // Read number of partitions
+        // Read parameters
         int L = Integer.parseInt(args[1]);
         int K = Integer.parseInt(args[2]);
         int M = Integer.parseInt(args[3]);
 
-        System.out.println("Command line arguments:");
-        for (int i = 0; i<4; i++) {
-            System.out.println(args[i]);
-        }
+        //Print parameters
+        System.out.println("Input file = "+args[0]+", L = "+L+", K = "+K+", M = "+M);
 
-        //TODO: read the input and transform them into instances of class Vector
-
+        //TODO: read the input and transform them into instances of class Vector.
+        // DO WE NEED TO PUT REPARTITION ON TEXT FILE OR INPUT POINTS? In teoria è uguale, dopo pure inputPoints è su 5 partitions
 
         // Read input file and subdivide it into L random partitions
-        JavaRDD<String> inputPoints = sc.textFile(args[0]).repartition(L).cache();
+        JavaRDD<String> data = sc.textFile(args[0]).repartition(L);
+
+        //This should be done locally
+        JavaPairRDD<Boolean, Vector> inputPoints = data.mapToPair((x) -> {
+            String[] point = x.split(",");
+            double[] values = new double[point.length-1];
+            for (int i = 0; i< point.length-1; i++) {
+                values[i] = Double.parseDouble(point[i]);
+            }
+            return new Tuple2<Boolean, Vector>(point[point.length-1].equals("A"), Vectors.dense(values));
+        }).cache();
         // textFile read the file and transforms it into an RDD of strings (elements = lines).
         // Repartition forces spark to partition in L groups (at random).
         // Cache says that if Spark has to materialize it, it will store in RAM if there is space.
@@ -69,43 +81,50 @@ public class G36HW1 {
         // SETTING GLOBAL VARIABLES
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        long N, NA, NB;
+        long N, NA = 0, NB = 0;
         //TODO: calculate and store NA and NB and print them
         N = inputPoints.count(); //With this we are storing the RDD
-        System.out.println("N = " + N);
+
+        Map<Boolean, Long> counts = inputPoints.countByKey();
+        NA = counts.get(true);
+        NB = counts.get(false);
+
+        System.out.println("N = "+N+", NA = "+NA+", NB = "+NB);
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // LLOYD'S ALGORITHM INVOCATION
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        //TODO: invoke the built-in Lloyd's algorithm with M iterations
+        //TODO: invoke the built-in Lloyd's algorithm with M iterations and K clusters. IS IT RIGHT?
+        KMeansModel clusters = KMeans.train(inputPoints.map(x -> x._2).rdd(), K, M);
 
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // CALL OBJECTIVE FUNCTIONS
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        double standardObjective = MRComputeStandardObjective();
-        double fairObjective = MRComputeFairObjective();
-        System.out.println("Standard objective: "+standardObjective);
-        System.out.println("Fair objective: "+fairObjective);
+        double standardObjective = MRComputeStandardObjective(inputPoints, clusters.clusterCenters());
+        double fairObjective = MRComputeFairObjective(inputPoints, clusters.clusterCenters());
+        System.out.println("Delta(U,C) = "+standardObjective);
+        System.out.println("Phi(A,B,C) = "+fairObjective);
 
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
         // CALL MRPRINTSTATISTICS
         // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
-        MRPrintStatistics();
+        MRPrintStatistics(inputPoints, clusters.clusterCenters());
     }
 
-    public static double MRComputeStandardObjective() { //Input is an RDD and set of centroids
+    public static double MRComputeStandardObjective(JavaPairRDD<Boolean, Vector> rdd, Vector[] centroids) { //Input is an RDD and set of centroids
         return 0.0;
     }
 
-    public static double MRComputeFairObjective() { //Input is an RDD and set of centroids
+    public static double MRComputeFairObjective(JavaPairRDD<Boolean, Vector> rdd, Vector[] centroids) { //Input is an RDD and set of centroids
         return 0.0;
     }
 
-    public static void MRPrintStatistics() { //Input is an RDD and set of centroids
+    public static void MRPrintStatistics(JavaPairRDD<Boolean, Vector> rdd, Vector[] centroids) { //Input is an RDD and set of centroids
 
     }
+
 }
